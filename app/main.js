7802d2b852545322dff6f4d03d833bd54e558fa6
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-08-15 16:47:14 +0300

    feat: Add HTTP response handling to server

--- app/main.js
+++ app/main.js
@@ -5,12 +5,74 @@ console.log('Logs from your program will appear here!')
 
 const CRLF = '\r\n'
 
+const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+const httpVersions = ['HTTP/1.1', 'HTTP/2']
+
 const OK = () => 'HTTP/1.1 200 OK' + CRLF + CRLF
+const E404 = () => 'HTTP/1.1 404 Not Found' + CRLF + CRLF
+
+const parseRequestHeaders = (headers = '') => {
+   const parts = headers.split(CRLF)
+
+   const requestLine = parts?.[0]
+   if (!requestLine) throw new Error('Empty request headers')
+
+   const requestLineParts = requestLine.split(' ')
+
+   const method = requestLineParts?.[0]?.toUpperCase?.()
+   if (!method || !methods.includes(method))
+      throw new Error('Illegal method: ' + method)
+
+   const requestUri = requestLineParts?.[1]
+   if (!requestUri || !requestUri.startsWith('/'))
+      throw new Error('Illegal request: ' + requestUri)
+
+   const httpVersion = requestLineParts?.[2]
+   if (!httpVersion || !httpVersions.includes(httpVersion))
+      throw new Error('Unsupported http version: ' + httpVersion)
+
+   const host = requestLine?.[1]
+   if (!host) throw new Error('Unknown host')
+
+   const userAgent = requestLine?.[2]
+   if (!userAgent) throw new Error('Unknown user agent')
+
+   const accepts = requestLine?.[3]
+   if (!accepts) throw new Error('Unknown Accepts param')
+
+   return {
+      method,
+      requestUri,
+      httpVersion,
+      host,
+      userAgent,
+      accepts,
+   }
+}
+
+const send = (socket, data) =>
+   new Promise((resolve) => {
+      socket.write(data, () => {
+         socket.end()
+         return resolve()
+      })
+   })
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
    console.log('connection open')
-   socket.write(OK())
+   //    socket.write(OK())
+   socket.on('data', async (data) => {
+      console.log('DATA', JSON.stringify(data.toString()))
+
+      const headers = parseRequestHeaders(data.toString())
+
+      if (headers.requestUri === '/') {
+         await send(socket, OK())
+      } else {
+         await send(socket, E404())
+      }
+   })
    socket.on('close', () => {
       console.log('connection closed')
       socket.end()
